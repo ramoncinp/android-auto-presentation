a7fe1c4d10c345f5d0a2dcc936e6867777f9b1ce
Aldair Parra <aldairparrag@gmail.com>
2022-05-12 18:00:47 -0500

    Create main page

---
+++ app/src/main/java/com/ramoncinp/androidautopresentation/ui/GreetingPage.kt
@@ -0,0 +1,12 @@
+package com.ramoncinp.androidautopresentation.ui
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GreetingPage() {
+
+    Scaffold {
+
+    }
+}
--- app/src/main/java/com/ramoncinp/androidautopresentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/ramoncinp/androidautopresentation/ui/main/MainActivity.kt
@@ -1,20 +1,25 @@
-package com.ramoncinp.androidautopresentation
+package com.ramoncinp.androidautopresentation.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.ramoncinp.androidautopresentation.R
 import com.ramoncinp.androidautopresentation.ui.theme.AndroidAutoPresentationTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,21 +57,25 @@ fun MainContent() {
     Scaffold {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.primary) {
             Box(contentAlignment = Alignment.Center) {
-                Text(text = "Hello!")
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(
+                        painterResource(id = R.mipmap.ic_launcher_round),
+                        stringResource(id = R.string.app_name),
+                    )
+                    CircularProgressIndicator(
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 40.dp)
+                    )
+                }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AndroidAutoPresentationTheme {
-        Greeting("Android")
+        MainContent()
     }
 }
