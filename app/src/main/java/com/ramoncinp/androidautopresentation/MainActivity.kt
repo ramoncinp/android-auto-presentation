67dfba34774a4a9085148e603581c174515b7587
Aldair Parra <aldairparrag@gmail.com>
2022-05-12 17:33:09 -0500

    Apply theme to app

--- app/src/main/java/com/ramoncinp/androidautopresentation/MainActivity.kt
+++ app/src/main/java/com/ramoncinp/androidautopresentation/MainActivity.kt
@@ -3,13 +3,18 @@ package com.ramoncinp.androidautopresentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramoncinp.androidautopresentation.ui.theme.AndroidAutoPresentationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +22,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidAutoPresentationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun SetUiColors() {
+    val systemUiController = rememberSystemUiController()
+    val color = MaterialTheme.colors.primary
+
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = color,
+            darkIcons = false
+        )
+    }
+}
+
+@Composable
+fun MyApp() {
+    SetUiColors()
+    MainContent()
+}
+
+@Composable
+fun MainContent() {
+    Scaffold {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.primary) {
+            Box(contentAlignment = Alignment.Center) {
+                Text(text = "Hello!")
             }
         }
     }
--- app/src/main/java/com/ramoncinp/androidautopresentation/ui/theme/Color.kt
+++ app/src/main/java/com/ramoncinp/androidautopresentation/ui/theme/Color.kt
@@ -6,3 +6,11 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val PrimaryColor = Color(0xFF2e7c31)
+val PrimaryLightColor = Color(0xFF60ac5d)
+val PrimaryDarkColor = Color(0xFF004f04)
+
+val SecondaryColor = Color(0xFF1b5e20)
+val SecondaryLightColor = Color(0xFF4c8c4a)
+val SecondaryDarkColor = Color(0xFF003300)
